cb8689b5d5f93a8df8b5fd4b449535ff9bf233a8
Lui Habl <txaicalvoso@gmail.com>
2023-02-13 00:22:48 +0100

    Quick test

--- src/osemu.py
+++ src/osemu.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/<int:number>/')
+def display(number):
+    return f"<p>You requested {number}!</p>"
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World 222!</p>"
