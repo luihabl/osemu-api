549055f2f5fda260b433aa3f572f111a1d3ea9cb
Lui Habl <txaicalvoso@gmail.com>
2023-02-15 07:32:50 +0100

    Handling request data

--- src/osemu/api/consoles.py
+++ src/osemu/api/consoles.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .models import Console
 from ..extensions import db
 
 consoles_bp = Blueprint('consoles', __name__, url_prefix='/consoles')
 
-@consoles_bp.route('/show')
+@consoles_bp.route('/', methods=['GET'])
 def show_stuff():
     consoles = Console.query.all()
-    cd = ['a']
+    cd = []
     for console in consoles:
         cd.append({
             'name': console.name,
@@ -17,9 +17,15 @@ def show_stuff():
     return cd
 
 
-@consoles_bp.route('/add/<name>')
-def add_stuff(name):
-    console = Console(id=None, name=name, manufacturer='Sony')
+@consoles_bp.route('/', methods=['POST'])
+def add_stuff():
+
+    data = request.get_json()
+
+    name = data['name']
+    manufacturer = data['manufacturer']
+
+    console = Console(id=None, name=name, manufacturer=manufacturer)
     db.session.add(console)
     db.session.commit()
     return f'Console added [{console.name}]'
