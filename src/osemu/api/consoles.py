e6dec849cb6717258a2fbb8e779cc8c24852be15
Lui Habl <txaicalvoso@gmail.com>
2023-02-15 22:24:42 +0100

    Filtering

--- src/osemu/api/consoles.py
+++ src/osemu/api/consoles.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import func
 
 from .models import Console
 from .schema import ConsoleSchema
@@ -8,28 +9,48 @@ from ..extensions import db
 
 consoles_bp = Blueprint('consoles', __name__, url_prefix='/consoles')
 
+def filter_wild(query, par_list, data):
+    for key in par_list:
+        if key in data:
+            val = data[key]
+            attr = getattr(Console, key)
+            query = query.filter(attr.ilike(f'%{val}%'))
+    return query
+
+def filter_exact(query, par_list, data):
+    for key in par_list:
+        if key in data:
+            val = data[key]
+            attr = getattr(Console, key)
+            query = query.filter(attr == val)
+    return query
+
 @consoles_bp.route('/', methods=['GET'])
-def show_stuff():
-    consoles = Console.query.all()
-    cd = []
-    for console in consoles:
-        cd.append({
-            'name': console.name,
-            'manufacturer': console.manufacturer
-        })
+def get_consoles():
+    data = request.values.to_dict()
 
-    return cd
+    consoles = filter_wild(db.session.query(Console), 
+                          ['name', 'manufacturer'], data)
+    consoles = filter_exact(consoles, ['id'], data)
+    consoles = consoles.all()
 
+    if len(consoles) == 1:
+        return ConsoleSchema().dump(consoles[0])
+    return ConsoleSchema(many=True).dump(consoles) 
 
 @consoles_bp.route('/', methods=['POST'])
-def new_console():
+def add_console():
 
     json_data = request.get_json()
     if not json_data:
         return {"message": "No input data provided"}, 400  
 
     try:
-        console = ConsoleSchema(load_instance=True).load(json_data)
+        if isinstance(json_data, list):
+            console = ConsoleSchema(load_instance=True, many=True).load(json_data)
+        elif isinstance(json_data, dict):
+            console = ConsoleSchema(load_instance=True, many=False).load(json_data)
+
     except ValidationError as err:
         return err.messages, 422
 
--- src/osemu/api/models.py
+++ src/osemu/api/models.py
@@ -1,12 +1,13 @@
 from ..extensions import db
 from sqlalchemy.orm import validates
 from sqlalchemy.schema import CheckConstraint
-
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 
 class Console(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(255), nullable=False, unique=True)
     manufacturer = db.Column(db.String(255), nullable=False)
 
