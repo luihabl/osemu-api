d9f361c96bdb5cfc1abd198e42407e7784ac5797
Lui Habl <txaicalvoso@gmail.com>
2023-02-16 12:38:53 +0100

    Put and patch methods

--- src/osemu/api/consoles.py
+++ src/osemu/api/consoles.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import func
 
 from .models import Console
 from .schema import ConsoleSchema
@@ -14,7 +13,7 @@ def filter_wild(query, par_list, data):
         if key in data:
             val = data[key]
             attr = getattr(Console, key)
-            query = query.filter(attr.ilike(f'%{val}%'))
+            query = query.where(attr.ilike(f'%{val}%'))
     return query
 
 def filter_exact(query, par_list, data):
@@ -22,17 +21,20 @@ def filter_exact(query, par_list, data):
         if key in data:
             val = data[key]
             attr = getattr(Console, key)
-            query = query.filter(attr == val)
+            query = query.where(attr == val)
     return query
+
+def all_as_list(q):
+    return [it for (it,) in q.all()]
 
 @consoles_bp.route('/', methods=['GET'])
 def get_consoles():
     data = request.values.to_dict()
 
-    consoles = filter_wild(db.session.query(Console), 
+    consoles = filter_wild(db.select(Console), 
                           ['name', 'manufacturer'], data)
     consoles = filter_exact(consoles, ['id'], data)
-    consoles = consoles.all()
+    consoles = all_as_list(db.session.execute(consoles))
 
     if len(consoles) == 1:
         return ConsoleSchema().dump(consoles[0])
@@ -50,6 +52,8 @@ def add_console():
             console = ConsoleSchema(load_instance=True, many=True).load(json_data)
         elif isinstance(json_data, dict):
             console = ConsoleSchema(load_instance=True, many=False).load(json_data)
+        else:
+            raise ValidationError
 
     except ValidationError as err:
         return err.messages, 422
@@ -64,3 +68,33 @@ def add_console():
         return f'{err.orig}', 422
 
     return f'Console added [{json_data}]'
+
+
+@consoles_bp.route('/<id>', methods=['GET'])
+def get_console_by_id(id):
+        console = db.get_or_404(Console, id)
+        return ConsoleSchema().dump(console)
+
+
+@consoles_bp.route('/<id>', methods=['PATCH', 'PUT'])
+def update_console(id):
+
+    console = db.get_or_404(Console, id)
+
+    json_data = request.get_json()
+    if not json_data:
+        return {"message": "No input data provided"}, 400  
+
+    partial = request.method == 'PATCH'
+    
+    try:
+        new_entry = ConsoleSchema().load(json_data, partial=partial)
+    except ValidationError as err:
+        return err.messages, 422
+
+    new_entry['id'] = console.id
+    for k in new_entry.keys():
+        setattr(console, k, new_entry[k])
+    db.session.commit()
+
+    return "Entry updated successfuly."
