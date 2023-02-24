d9c858b35c9a2d3c16c57a8e7bf456f6ab4ab247
Lui Habl <txaicalvoso@gmail.com>
2023-02-24 16:15:25 -0300

    Adding other models

--- src/osemu/api/models.py
+++ src/osemu/api/models.py
@@ -22,45 +22,60 @@ class Console(db.Model):
     name = db.Column(db.String(255), nullable=False, unique=True)
     company_id = db.Column(db.UUID, db.ForeignKey('company.id'), nullable=False)
     company = db.relationship('Company', back_populates='consoles')
+    release_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    short_description = db.Column(db.Text)
+    wiki_url = db.Column(db.String(512))
+    image_url = db.Column(db.String(512))
 
-    # @validates('name')
-    # def validate_name(self, key, name):
-    #     if len(name) <= 2:
-    #         raise ValueError('Console name must have at least two characters.')
-    #     return name 
-
-    # @validates('company')
-    # def validate_company(self, key, company):
-    #     if len(company) <= 2:
-    #         raise ValueError('company name must have at least two characters.')
-    #     return company 
-
-    # __table_args__ = (
-    #     CheckConstraint('char_length(name) > 2',
-    #                     name='name_min_length'),
-    #     CheckConstraint('char_length(company) > 2',
-    #                     name='company_min_length')
-    # )
     def __repr__(self) -> str:
         return f'Console(id={self.id}, name={self.name})'
 
 
-class ProgrammingLanguage(db.Model):
+class EmulatorLanguage(db.Model):
+    __tablename__ = 'emulator_language'
+
+    amount = db.Column(db.Float, nullable=False)
+
+    emulator_id = db.Column(db.UUID, db.ForeignKey('emulator.id'), primary_key=True)
+    language_id = db.Column(db.UUID, db.ForeignKey('language.id'), primary_key=True)
+    
+    emulator = db.relationship('Emulator', back_populates='languages')
+    language = db.relationship('Language', back_populates='emulators')
+
+
+class Language(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(255), nullable=False, unique=True)
+
+    emulators = db.relationship('EmulatorLanguage', back_populates='language')
 
 
 class License(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(255), nullable=False, unique=True)
     url = db.Column(db.String(255), nullable=False, unique=True)
+    emulators = db.relationship('Emulator', back_populates='license')
 
 
 class Emulator(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(255), nullable=False, unique=True)  
-    git_url = db.Column(db.String(255), nullable=True) 
+    git_url = db.Column(db.String(512))
+    gh_stars = db.Column(db.Integer)
+    website_url = db.Column(db.String(512))
+    image_url = db.Column(db.String(512)) 
+
     consoles = db.relationship('Console', secondary=emu_console, backref='emulators')
 
+    license_id = db.Column(db.UUID, db.ForeignKey('license.id'))
+    license = db.relationship('License', back_populates='emulators')
+
+    short_description = db.Column(db.Text)
+    latest_update = db.Column(db.DateTime)
+    release_date = db.Column(db.Date)
+
+    languages = db.relationship('EmulatorLanguage', back_populates='emulator')
+    
     def __repr__(self) -> str:
         return f'Emulator(id={self.id}, name={self.name})'
--- src/osemu/api/schema.py
+++ src/osemu/api/schema.py
@@ -1,26 +1,50 @@
 from marshmallow import Schema, fields, ValidationError, EXCLUDE
-from .models import Console, Emulator, Company
+from osemu.api import models
 
 def must_not_be_blank(data):
     if not data:
         raise ValidationError("Data not provided.")
 
 class CompanySchema(Schema):
-    model = Company
+    model = models.Company
     id = fields.UUID(dump_only=True)
     name = fields.String(required=True, validate=must_not_be_blank)
 
 class ConsoleSchema(Schema):
-    model = Console
+    model = models.Console
     id = fields.UUID(dump_only=True)
     name = fields.String(required=True, validate=must_not_be_blank)
     company = fields.Nested(CompanySchema, unknown=EXCLUDE)
+    release_date = fields.Date()
+    end_date = fields.Date()
+    short_description = fields.String()
+    wiki_url = fields.String()
+    image_url = fields.String()
 
-class EmulatorSchema(Schema):
-    model = Emulator
+class LanguageSchema(Schema):
+    model = models.Language
     id = fields.UUID(dump_only=True)
     name = fields.String(required=True, validate=must_not_be_blank)
-    git_url = fields.String(required=False)
-    consoles = fields.Nested(ConsoleSchema, many=True, unknown=EXCLUDE)
 
+class LicenseSchema(Schema):
+    model = models.License
+    id = fields.UUID(dump_only=True)
+    name = fields.String()
+    url = fields.String()
+
+class EmulatorSchema(Schema):
+    model = models.Emulator
+    id = fields.UUID(dump_only=True)
+    name = fields.String(required=True, validate=must_not_be_blank)
+    git_url = fields.String()
+    gh_stars = fields.Integer()
+    website_url = fields.String()
+    image_url = fields.String()
+    consoles = fields.Nested(ConsoleSchema, many=True, unknown=EXCLUDE)
+    license = fields.Nested(LicenseSchema, unknown=EXCLUDE)
+    short_description = fields.String()
+    latest_update = fields.DateTime()
+    release_date = fields.Date()
+    languages = fields.Nested(LanguageSchema, many=True, unknown=EXCLUDE)
+    
 
