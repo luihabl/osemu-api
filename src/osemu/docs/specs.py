7a2b08e1741ea9bd7afd7071539d97cfcf0a30f9
Lui Habl <txaicalvoso@gmail.com>
2023-03-07 09:09:43 -0300

    Adding api specs

--- src/osemu/docs/specs.py
+++ src/osemu/docs/specs.py
@@ -12,9 +12,9 @@ spec = APISpec(
 
 # Marshmallow schemas
 from osemu.api import schema
+spec.components.schema("Languages", schema=schema.LanguageSchema)
 spec.components.schema("Company", schema=schema.CompanySchema)
 spec.components.schema("Console", schema=schema.ConsoleSchema)
-spec.components.schema("Languages", schema=schema.LanguageSchema)
 spec.components.schema("Emulator", schema=schema.EmulatorSchema)
 spec.components.schema("User", schema=schema.UserSchema)
 
@@ -24,8 +24,35 @@ spec.components.security_scheme("cookieAuth", api_key_scheme)
 
 # API views
 def register_views_on_spec():
+
+    # Authentication
     from osemu.api.views import auth
     spec.path(view=auth.get_user)
     spec.path(view=auth.signup)
     spec.path(view=auth.login)
     spec.path(view=auth.logout)
+
+    # Emulators
+    from osemu.api.views import emulators
+    spec.path(view=emulators.emulator_entry_view)
+    spec.path(view=emulators.emulator_group_view)
+
+    # Consoles
+    from osemu.api.views import consoles
+    spec.path(view=consoles.console_entry_view)
+    spec.path(view=consoles.console_group_view)
+
+    # Company
+    from osemu.api.views import companies
+    spec.path(view=companies.company_entry_view)
+    spec.path(view=companies.company_group_view)
+
+    # Languages
+    from osemu.api.views import languages
+    spec.path(view=languages.language_entry_view)
+    spec.path(view=languages.language_group_view)
+
+    # Licenses
+    from osemu.api.views import licenses
+    spec.path(view=licenses.license_entry_view)
+    spec.path(view=licenses.license_group_view)
